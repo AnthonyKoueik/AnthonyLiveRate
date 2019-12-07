d272ff8bf90df7c6891cffab56e4eab97cd9ee5c
Anthony-Home <anthony.koueik@gmail.com>
2019-12-07 13:08:20 +0200

    adding calculator Unit Tests

---
+++ app/src/test/java/com/anthony/revolut/util/CalculateTest.kt
@@ -0,0 +1,47 @@
+package com.anthony.revolut.util
+
+import com.anthony.revolut.utils.calculate
+import junit.framework.TestCase.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import java.util.*
+
+
+/**
+ * Created by Anthony Koueik on 12/7/2019.
+ * KOA
+ * anthony.koueik@gmail.com
+ */
+class CalculateTest {
+
+
+    // mocks rates
+    private val rateForUSD = HashMap<String, Double>()
+        .apply { put("EUR", 0.86195) }
+        .apply { put("GBP", 0.77424) }
+
+
+    @Test
+    fun `GivenEmptyMap-WhenCurrencyProvided-Returns-NoResult`() {
+        val currencyValue = calculate(emptyMap(),
+            Currency.getInstance("EUR"),
+            100.0)
+        assertNull(currencyValue)
+    }
+
+    @Test
+    fun `GivenUsdBasedRates-WhenCurrencyProvidedIsEUR-Returns-ValidResults`() {
+        val currencyValue = calculate(rateForUSD,
+            Currency.getInstance("EUR"),
+            1.00)
+        assertEquals(0.86195, currencyValue?.rate)
+    }
+
+    @Test
+    fun `GivenZeroExchange-WhenCurrencyProvidedIsEUR-Returns-ZeroAsResult`() {
+        val currencyValue = calculate(rateForUSD,
+            Currency.getInstance("EUR"),
+             0.0)
+        assertEquals(0.0, currencyValue?.rate)
+    }
+}
